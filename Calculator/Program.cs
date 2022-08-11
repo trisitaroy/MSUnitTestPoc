03b5ec7fd283d8a979cbc5e70bdb80f7e2470730
Trisita Roy <Trisita.Roy@gds.ey.com>
2022-08-11 16:06:33 +0530

    Added test proj

---
+++ Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //Addition  
+        public  int Addition(int input_1, int input_2)
+        {
+            int result = input_1 + input_2;
+            return result;
+        }
+        //Substraction  
+        public  int Subtraction(int input_1, int input_2)
+        {
+            int result = input_1 - input_2;
+            return result;
+        }
+        //Multiplication  
+        public  int Multiplication(int input_1, int input_2)
+        {
+            int result = input_1 + input_2;
+            return result;
+        }
+        //Division  
+        public  int Division(int input_1, int input_2)
+        {
+            int result = input_1 + input_2;
+            return result;
+        }
+    }
+}
---
+++ TestProject/ProgramTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void AdditionTest()
+        {
+            //Arrange
+            Program prog = new Program();
+            int expectedResult = 5;
+            int input1 = 2, input2 = 3;
+
+            //Act
+            int result = prog.Addition(input1, input2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+
+        }
+
+        [TestMethod()]
+        public void SubtractionTest()
+        {
+            //Arrange
+            Program prog = new Program();
+            int expectedResult = 1;
+            int input1 = 5, input2 = 4;
+
+            //Act
+            int result = prog.Subtraction(input1, input2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+
+        }
+    }
+}
